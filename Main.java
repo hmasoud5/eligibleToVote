f9c6f2f81f7501a868c699b094e342b8d3107f1f
Heba Masoud <heba.masoud@patelhs.org>
2020-10-11 15:00:34 +0000

    added third commit

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ class Main {
       //if age is less than 18 you cannot vote
       //AND if age is less than or equal to 17 you still cannot vote
       System.out.println("You are old enough to vote");
+      //You are old enough to vote if you are 18 or older
     }else {
       System.out.println("You are too young to vote if age is...");
     }
