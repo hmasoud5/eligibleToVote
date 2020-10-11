f7d1a2b64c9c30613f116092dc3eeab3c01a5d79
Heba Masoud <heba.masoud@patelhs.org>
2020-10-11 15:06:00 +0000

    added not equal

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String[] args) {
     int age = 18;
     
-    if (age < 18 && age <= 17 ){
+    if (age < 18 && age != 18 ){
       //if age is less than 18 you cannot vote
       //AND if age is less than or equal to 17 you still cannot vote
       System.out.println("You are old enough to vote");
