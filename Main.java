7c68de97b193aaec39faf3a2f74ed70f1f9c2244
Heba Masoud <heba.masoud@patelhs.org>
2020-10-11 14:51:36 +0000

    switched print statements

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@ class Main {
   public static void main(String[] args) {
     int age = 18;
     
-    if (age > 18 && age >= 18 ){
-      System.out.println("You are too young to vote");
-    }else {
+    if (age < 18 && age <= 18 ){
       System.out.println("You are old enough to vote");
+    }else {
+      System.out.println("You are too young to vote");
     }
-
+    for (int a = 1; a < 18; a++){
+      System.out.println(a + "");
+    }
   }
 }
