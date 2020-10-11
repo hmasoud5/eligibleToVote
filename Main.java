ecd2d83d4f2f75c665397ee7224dba6d034da79d
Heba Masoud <heba.masoud@patelhs.org>
2020-10-11 14:52:45 +0000

    added "if age is..."

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ class Main {
     if (age < 18 && age <= 18 ){
       System.out.println("You are old enough to vote");
     }else {
-      System.out.println("You are too young to vote");
+      System.out.println("You are too young to vote if age is...");
     }
     for (int a = 1; a < 18; a++){
       System.out.println(a + "");
