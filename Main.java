2fa8b42bf849858817f5aeffaad45b3ae786aeea
Heba Masoud <heba.masoud@patelhs.org>
2020-10-11 14:28:16 +0000

    added if else statement

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int age = 18;
+    
+    if (age > 18 && age >= 18 ){
+      System.out.println("You are too young to vote");
+    }else {
+      System.out.println("You are old enough to vote");
+    }
+
   }
 }
