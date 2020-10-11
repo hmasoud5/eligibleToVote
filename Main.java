6cdc313745e16eaa457ad7bd3a14312002d9e5d3
Heba Masoud <heba.masoud@patelhs.org>
2020-10-11 15:03:04 +0000

    added more comments

--- Main.java
+++ Main.java
@@ -9,9 +9,12 @@ class Main {
       //You are old enough to vote if you are 18 or older
     }else {
       System.out.println("You are too young to vote if age is...");
+      //You are too young to vote if the if statement says age is less than 18 or less than or equal to 17
     }
     for (int a = 1; a < 18; a++){
+      //Ages 1 to 17 cannot vote since if else statement printed "You are too young to vote if age is"
       System.out.println(a + "");
+      //age will accelerate to the number 17
     }
   }
 }
