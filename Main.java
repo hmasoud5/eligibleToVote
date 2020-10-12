12be327e617b2ec7075995720e8b38546f078413
Heba Masoud <heba.masoud@patelhs.org>
2020-10-12 12:42:53 +0000

    changed print statement

--- Main.java
+++ Main.java
@@ -7,10 +7,10 @@ class Main {
     if (age < 18 && age != 18 ){
       //if age is less than 18 you cannot vote
       //AND if age is less than or equal to 17 you still cannot vote
-      System.out.println("You are old enough to vote");
+      System.out.println("You are eligble to vote");
       //You are old enough to vote if you are 18 or older
     }else {
-      System.out.println("You are too young to vote if age is...");
+      System.out.println("You are not eligble to vote if age is...");
       //You are too young to vote if the if statement says age is less than 18 or less than or equal to 17
     }
     for (int a = 1; a < 18; a++){
